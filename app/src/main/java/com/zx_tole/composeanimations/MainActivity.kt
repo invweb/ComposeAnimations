430c7ba8deae3ddbe3ea8d72b001eb8d4e10fd7e
Vasily <invweb@gmail.com>
2022-12-31 18:12:50 +0300

    init

--- app/src/main/java/com/zx_tole/composeanimations/MainActivity.kt
+++ app/src/main/java/com/zx_tole/composeanimations/MainActivity.kt
@@ -3,13 +3,21 @@ package com.zx_tole.composeanimations
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
 import com.zx_tole.composeanimations.ui.theme.ComposeAnimationsTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +30,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    Circle()
                 }
             }
         }
@@ -30,14 +38,31 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
+fun Circle() {
+    val animationTargetState = remember { mutableStateOf(0f) }
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    ComposeAnimationsTheme {
-        Greeting("Android")
+    val animatedFloatState = animateFloatAsState(
+        // Whenever the target value changes, new animation
+        // will start to the new target value
+        targetValue = animationTargetState.value,
+        animationSpec = tween(durationMillis = 3000)
+    )
+
+    Canvas(
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight()
+            .background(Color.Black)
+            .clickable {
+                // Change the target state to start the animation
+                animationTargetState.value = 1f
+            }
+    ) {
+        drawCircle(
+            color = Color.White,
+            radius = 50f,
+            // The circle will fade in with the given animation value
+            alpha = animatedFloatState.value
+        )
     }
 }
